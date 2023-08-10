1413d0e7d9aa8e00a24016e9fcb5ee0b339ec3f5
krishilsheth404 <97043368+krishilsheth404@users.noreply.github.com>
2023-08-10 12:00:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,43 +33,38 @@ app.post('/', (req, res) => {
 
 app.post('/result', async (req, res) => {
     // Insert Login Code Here   
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+        channel: 'chrome',
+        
 
-        const browser = await puppeteer.launch({ headless: false });
+    })
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1240,
+        height: 880,
+        deviceScaleFactor: 1,
+      });
+
+    await page.goto(`https://www.netmeds.com/prescriptions/crocin-650mg-tablet-15-s`);
     
-        const page = await browser.newPage();
-        await page.goto('https://quotes.toscrape.com/search.aspx');
-    
-        await page.waitForSelector('#author');
-        await page.select('#author', 'Albert Einstein');
-    
-        await page.waitForSelector('#tag');
-        await page.select('#tag', 'learning');
-    
-        await page.click('.btn');
-        await page.waitForSelector('.quote');
-    
-        // extracting information from code
-        let quotes = await page.evaluate(() => {
-    
-            let quotesElement = document.body.querySelectorAll('.quote');
-            let quotes = Object.values(quotesElement).map(x => {
-                return {
-                    author: x.querySelector('.author').textContent ?? null,
-                    quote: x.querySelector('.content').textContent ?? null,
-                    tag: x.querySelector('.tag').textContent ?? null,
-    
-                }
-            });
-    
-            return quotes;
-    
+    try {
+        await page.evaluate(() => {
+            document.querySelector('button[title="ADD TO CART"]').click();
         });
-    
-        // logging results
-        console.log(quotes);
-        await browser.close();
-    
-    
+        await page.evaluate(() => {
+            document.querySelector('button[class="mc_cartBtn"]').click();
+        });
+        console.log("Added to cart");
+        await page.waitForTimeout(1000);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 const port = process.env.PORT || 3000 // Port we will listen on
